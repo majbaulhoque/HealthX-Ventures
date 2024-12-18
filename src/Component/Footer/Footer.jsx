29537f16fb1c0ba1a4b53dd735677a507f27b8e8
Majba UL Hoque <majba9399@gmail.com>
2024-12-18 13:03:11 +0600

    complete footer part

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,9 +1,113 @@
+import { FaFacebookF, FaTwitter, FaYoutube, FaPinterestP, FaChevronRight } from "react-icons/fa";
+import "./Footer.css";
+import Logo from "../../assets/Logo/Logo";
 
 const Footer = () => {
     return (
-        <div>
-            fff
-        </div>
+        <footer className="footer">
+            <div className="container">
+                <div className="footer-section">
+                    <div className="footer-logo-parent">
+                        <Logo className="footer-image" />
+                        <h2 className="footer-logo">Health</h2>
+                    </div>
+                    <address>
+                        <p>Sahibabad, Ghaziabad, Uttar Pradesh 201005</p>
+                        <p>+1-887*****78787</p>
+                        <p>
+                            <a href="mailto:info@demohelp.com" className="footer-link">
+                                info@demohelp.com
+                            </a>
+                        </p>
+                    </address>
+                    <div className="footer-socials">
+                        <a href="#" aria-label="Facebook" className="social-icon">
+                            <FaFacebookF />
+                        </a>
+                        <a href="#" aria-label="Twitter" className="social-icon">
+                            <FaTwitter />
+                        </a>
+                        <a href="#" aria-label="YouTube" className="social-icon">
+                            <FaYoutube />
+                        </a>
+                        <a href="#" aria-label="Pinterest" className="social-icon">
+                            <FaPinterestP />
+                        </a>
+                    </div>
+                </div>
+
+                {/* Quick Links */}
+                <div className="footer-section">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">About Us</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Our Pricing</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Our Gallery</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Appointment</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Departments */}
+                <div className="footer-section">
+                    <h4>Department</h4>
+                    <ul>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Orthology</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Neurology</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Dental Care</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Ophthalmology</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Cardiology</a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* Useful Links */}
+                <div className="footer-section">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">About Us</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Our Blog</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Our Gallery</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Appointment</a>
+                        </li>
+                        <li>
+                            <FaChevronRight className="li-icon" /> <a href="#" className="footer-link">Privacy Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Copyright */}
+            <div className="footer-bottom">
+                <p>&copy; 2024 Health.com. All Rights Reserved</p>
+            </div>
+        </footer>
     );
 };
 
--- src/Component/Services/Services.jsx
+++ src/Component/Services/Services.jsx
@@ -1,49 +1,58 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Logo from '/Image/commonLogo.png';
-import './Services.css'
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
-    const [isError, setIsError] = useState(['']);
-    const getServicesApi = async () =>{
+    const [isError, setIsError] = useState('');
+
+    const getServicesApi = async () => {
         try {
             const res = await axios.get('/services.json');
-            setServices(res.data)
-            console.log(res.data)
+            setServices(res.data);
         } catch (error) {
-            setIsError(error.message)
+            setIsError(error.message);
         }
-    } 
+    };
 
-    useEffect(() =>{
-        getServicesApi()
-    },[])
+    useEffect(() => {
+        getServicesApi();
+    }, []);
 
     return (
-        <div className="services-container">
-            <div className="services-header">
-                <img src={Logo} alt="Common Logo" />
-                <p>OUR SERVICES</p>
+        <div>
+            <div className="services-container">
+                <div className="services-header">
+                    <img src={Logo} alt="Common Logo" />
+                    <p>OUR SERVICES</p>
+                </div>
+                <h3 className="services-list">Our Mediax specialties service</h3>
+                {isError && <h2>{isError}</h2>}
+                <div className="services-grid">
+                    {services.map((service) => {
+                        const { id, name, description, img } = service;
+                        return (
+                            <div key={id} className="service-card">
+                                <img src={img} alt={name} />
+                                <h3>{name}</h3>
+                                <p>{description}</p>
+                                <button>Read More</button>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-            <h3 className="services-list">Our Mediax specialties service</h3>
-            {isError !== "" && <h2>{isError}</h2>}
-            <div className="services-grid">
-                {services?.map((service) => {
-                    const { id, name, description, img } = service || {};
-                    return (
-                        <div key={id} className="service-card">
-                            <img src={img} alt={name} />
-                            <h3>{name}</h3>
-                            <p>{description}</p>
-                            <button>Read More</button>
-                        </div>
-                    );
-                })}
+            <div className="services-explore">
+                <div className="explore-line"></div>
+                <div className="explore-content">
+                    You Get Our 20+ More services...
+                    <a href="#" className="explore-link">EXPLORE ALL SERVICES</a>
+                </div>
+                <div className="explore-line"></div>
             </div>
         </div>
     );
-    
 };
 
 export default Services;
