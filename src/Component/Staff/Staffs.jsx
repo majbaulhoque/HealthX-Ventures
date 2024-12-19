3dd8b3c6ccdda7a04be8661aa75911eef65f60ea
Majba UL Hoque <majba9399@gmail.com>
2024-12-19 11:33:46 +0600

    fixed special doctor's section

--- src/Component/Staff/Staffs.jsx
+++ src/Component/Staff/Staffs.jsx
@@ -22,37 +22,48 @@ const Staffs = () => {
     }, []);
 
     return (
-        <div className="doctor-container">
+        <div>
             <div className='doctor-header'>
-                <img src={CommonLogo} alt="" />
-                <p className='doctor-title'>OUR DOCTORS</p>
+                <img className='doctors-logo' src={CommonLogo} alt="" />
+                <p className='doctors-title'>OUR DOCTORS</p>
             </div>
             <h3 className='doctor-title'>Our Special Doctors</h3>
             <p className='doctor-description'>Our team of specialists is at the forefront of medical innovation. Each specialist brings a unique blend of expertise, empathy, and experience to ensure that your health is in the best hands:</p>
-            {isError !== '' && <h2>{isError}</h2>}
-            {doctors?.map((doctor) => {
-                const { id, name, specialty, rating, reviews, patients, hospital, image } = doctor || {};
-                return (
-                    <div key={id} className="doctor-card">
-                        <img src={image} alt={name} className="doctor-image" />
-                        <h2 className="doctor-name">{name}</h2>
-                        <div className="doctor-details">
-                            <p className="doctor-specialty">{specialty}</p>
-                            <div className="doctor-rating">
-                                <FaStar className="star-icon" color="#f5c518" />
-                                <span>
-                                    {rating} ({reviews})
-                                </span>
-                            </div>
-                        </div>
-                        <p className="doctor-patients">
-                            <strong>{patients} Patients</strong>
-                        </p>
-                        <p className="doctor-hospital">{hospital}</p>
-                    </div>
+            <div className="doctor-container">
 
-                );
-            })}
+                {isError !== '' && <h2>{isError}</h2>}
+                {doctors?.map((doctor) => {
+                    const { id, name, specialty, rating, reviews, patients, hospital, image } = doctor || {};
+                    return (
+                        <div key={id} className="doctor-card">
+                            <img src={image} alt={name} className="doctor-image" />
+                            <h2 className="doctor-name">{name}</h2>
+                            <div className="doctor-details">
+                                <p className="doctor-specialty">{specialty}</p>
+                                <div className="doctor-rating">
+                                    <FaStar className="star-icon" color="#f5c518" />
+                                    <span>
+                                        {rating} ({reviews})
+                                    </span>
+                                </div>
+                            </div>
+                            <p className="doctor-patients">
+                                <strong>{patients} Patients</strong>
+                            </p>
+                            <p className="doctor-hospital">{hospital}</p>
+                        </div>
+
+                    );
+                })}
+            </div>
+            <div className="services-explore">
+                <div className="explore-line"></div>
+                <div className="explore-content">
+                    You Get Our 20+ More services...
+                    <a href="#" className="explore-link">EXPLORE ALL SERVICES</a>
+                </div>
+                <div className="explore-line"></div>
+            </div>
         </div>
     );
 };
